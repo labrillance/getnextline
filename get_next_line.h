e5a71f00380c24010c170de603b13c4f578aa917
loris.briere <loriss@localhost.localdomain>
2020-01-02 16:09:37 +0100

    adding functions prototypes

--- get_next_line.h
+++ get_next_line.h
@@ -5,8 +5,16 @@
 ** get_next_line.h
 */
 
+#include <stdlib.h>
+#include <unistd.h>
+
 #ifndef GET_NEXT_LINE_
 
-#define READ_SIZE (1)
+#define READ_SIZE (5)
 
 #endif
+
+char *realloca(char *str, char *buff, int nb);
+char *first_alloc(int fd, int i, char *string, int w);
+char *get_next_line(int fd);
+int my_strlen(char *a);
