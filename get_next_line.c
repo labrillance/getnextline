e5d5077229288aaf9d84adc0229aae25d726c138
loris.briere <loriss@localhost.localdomain>
2020-01-02 15:56:06 +0100

    fix valgrind errors

--- get_next_line.c
+++ get_next_line.c
@@ -13,19 +13,21 @@
 
 char *realloca(char *str, char *buff, int nb)
 {
-    char *tmp;
+    char *tmp = NULL;
     int i = 0;
 
-    tmp = malloc(sizeof(char) * (READ_SIZE * nb + 2));
-    while (str != NULL && str[i] != 0) {
-        tmp[i] = str[i];
-        i++;
+    tmp = malloc(sizeof(char) * (READ_SIZE * nb + 1));
+    if (str != NULL) {
+        while (str[i] != 0) {
+            tmp[i] = str[i];
+            i++;
+        }
     }
     for (int x = 0; buff[x] != 0; x++) {
         tmp[i] = buff[x];
         i++;
     }
-    tmp[i + 1] = 0;
+    tmp[i] = 0;
     return tmp;
 }
 
@@ -34,7 +36,7 @@ char *first_alloc(int fd, int i, char *string, int w)
     char *buff = NULL;
 
     while (i != 0) {
-        buff = malloc(sizeof(char) * READ_SIZE * w);
+        buff = malloc(sizeof(char) * READ_SIZE * w + 1);
         if (!malloc)
             return NULL;
         i = read(fd, buff, i);
@@ -50,7 +52,7 @@ char *first_alloc(int fd, int i, char *string, int w)
 
 char *get_next_line(int fd)
 {
-    static int i = READ_SIZE;
+    static int i = 1;
     static int w = 1;
     static int cmp = 0;
     static char *string = NULL;
@@ -66,13 +68,14 @@ char *get_next_line(int fd)
         i = 0;
     }
     for (int tmp = cmp; string[tmp] != 0 && string[tmp] != '\n'; tmp++, x++);
-    buff = malloc(sizeof(char) * x);
+    buff = malloc(sizeof(char) * x + 1);
     for (int y = 0; string[cmp] != '\n' && string[cmp] != 0; y++, cmp++) {
         buff[y] = string[cmp];
     }
     buff[x] = '\0';
     if (string[cmp] == 0 && my_strlen(buff) == 0 )
-        buff = NULL;
-    cmp++;
+        return NULL;
+    if (string[cmp] != 0)
+        cmp++;
     return buff;
 }
